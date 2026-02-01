97df5e84b065e0fbb2b4e03c9f9d1d6dcd99e70c
Genevieve Krause <gkrause@i0n4.ocelote.hpc.arizona.edu>
2026-02-01 02:34:41 -0700

    Remove unused function p7_tophits_TabularXfam()

--- src/hmmer.h
+++ src/hmmer.h
@@ -2079,7 +2079,6 @@ extern int p7_tophits_Domains(FILE *ofp, P7_TOPHITS *th, P7_PIPELINE *pli, int t
 
 extern int p7_tophits_TabularTargets(FILE *ofp, char *qname, char *qacc, P7_TOPHITS *th, P7_PIPELINE *pli, int show_header);
 extern int p7_tophits_TabularExons(FILE *ofp, char *qname, char *qacc, P7_TOPHITS *th, P7_PIPELINE *pli, int show_header);
-extern int p7_tophits_TabularXfam(FILE *ofp, char *qname, char *qacc, P7_TOPHITS *th, P7_PIPELINE *pli);
 extern int p7_tophits_TabularTail(FILE *ofp, const char *progname, enum p7_pipemodes_e pipemode, 
           const char *qfile, const char *tfile, const ESL_GETOPTS *go);
 extern int p7_tophits_AliScores(FILE *ofp, char *qname, P7_TOPHITS *th );

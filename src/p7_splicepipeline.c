389283dff87758a6aa6223b6d8a7c073f66ff17b
Genevieve Krause <gkrause@junonia.hpc.arizona.edu>
2026-02-20 10:40:39 -0700

    Update splice signal probabilities so they sum to 1

--- src/p7_splicepipeline.c
+++ src/p7_splicepipeline.c
@@ -30,7 +30,7 @@
 void
 p7_splicepipeline_SignalScores(float *f)
 {
-  f[0] = log(0.9919);     /* GT-AG */
+  f[0] = log(0.9921);     /* GT-AG */
   f[1] = log(0.0073);     /* GC-AG */
   f[2] = log(0.0006);     /* AT-AC */
   return;

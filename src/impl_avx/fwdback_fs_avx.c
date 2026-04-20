8e5f345ee7c413daca5a0a008a214ac9deab855e
Genevieve Krause <genevievekrause@gmail.com>
2026-04-19 23:03:19 -0700

    Only zero out the zeroth row

--- src/impl_avx/fwdback_fs_avx.c
+++ src/impl_avx/fwdback_fs_avx.c
@@ -1392,13 +1392,12 @@ p7_Forward_Frameshift_avx(const ESL_DSQ *dsq, int L, const P7_FS_OPROFILE *om_fs
   fwd->totscale       = 0.0;
   zerov = _mm256_setzero_ps();
 
-  for (r = 0; r <= L; r++)
-    { __m256 *drow = fwd->dpf_avx[r];
-      for (q = 0; q < Q; q++)
-        MMO_FS(drow,q,p7X_FS_C0) = MMO_FS(drow,q,p7X_FS_C1) =
-        MMO_FS(drow,q,p7X_FS_C2) = MMO_FS(drow,q,p7X_FS_C3) =
-        MMO_FS(drow,q,p7X_FS_C4) = MMO_FS(drow,q,p7X_FS_C5) =
-        DMO_FS(drow,q)            = IMO_FS(drow,q)            = zerov; }
+  { __m256 *drow = fwd->dpf_avx[0];
+    for (q = 0; q < Q; q++)
+      MMO_FS(drow,q,p7X_FS_C0) = MMO_FS(drow,q,p7X_FS_C1) =
+      MMO_FS(drow,q,p7X_FS_C2) = MMO_FS(drow,q,p7X_FS_C3) =
+      MMO_FS(drow,q,p7X_FS_C4) = MMO_FS(drow,q,p7X_FS_C5) =
+      DMO_FS(drow,q)            = IMO_FS(drow,q)            = zerov; }
 
   for (r = 0; r < p7P_5CODONS; r++)
     for (q = 0; q < Q; q++)
